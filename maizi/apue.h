f66d9ab57e7ca255f88661f9e9a694bf659c8852
nameqiaohe <nameqiaohe@126.com>
2016-05-04 21:06:23 +0800

    add thread_id.c

---
+++ maizi/apue.h
@@ -0,0 +1,20 @@
+/*####################################################
+# File Name: apue.h
+# Author: YanWei
+# Email: nameqiaohe@126.com
+# Create Time: 2016-05-04 20:55:28
+# Last Modified: 2016-05-04 20:55:28
+####################################################*/
+
+#ifndef APUE_H
+#define APUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <assert.h>
+
+#endif//APUE_H
---
+++ maizi/thread_id.c
@@ -0,0 +1,39 @@
+/*####################################################
+# File Name: thread_id.c
+# Author: YanWei
+# Email: nameqiaohe@126.com
+# Create Time: 2016-05-04 20:57:32
+# Last Modified: 2016-05-04 20:57:32
+####################################################*/
+#include "apue.h"
+
+void print_id(const char *str){
+	pid_t pid;
+	pthread_t tid;
+
+	pid = getpid();
+	tid = pthread_self();
+
+	printf("%s pid = %u\t tid = %u\t tid = 0x%x\n", str, (unsigned int)pid, (unsigned int)tid, (unsigned int)tid);
+}
+
+void *thread_func(void *arg){
+	print_id("New Thread: ");
+	return (void *)0;
+}
+
+int main(void){
+	int err;
+	pthread_t tid;
+
+	err = pthread_create(&tid, NULL, thread_func, NULL);
+	assert(!err);
+
+	print_id("Main Thread: ");
+
+	int *retval;
+	pthread_join(tid, (void *)&retval);
+	printf("retval = %d\n", *((int *)&retval));
+
+	return 0;
+}
